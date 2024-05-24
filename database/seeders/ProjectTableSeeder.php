56175e9e15f16c9ad0ffdb6b2b9168a30f7966ff
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-05-24 18:31:04 +0200

    [Fatto Seed]

--- database/seeders/ProjectTableSeeder.php
+++ database/seeders/ProjectTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Project;
+use App\Models\Type;
 use App\Functions\Helper as Help;
 
 class ProjectTableSeeder extends Seeder
@@ -17,6 +18,7 @@ class ProjectTableSeeder extends Seeder
         $data = ['TITLE1', 'TITLE2', 'TITLE3', 'TITLE4', 'TITLE5'];
         foreach ($data as $item) {
             $new_item = new Project();
+            $new_item->type_id = Type::inRandomOrder()->first()->id;
             $new_item->title = $item;
             $new_item->slug = Help::generateSlug($item, Project::class);
             $new_item->save();
